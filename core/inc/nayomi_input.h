3dc994b4c7ebc904ae6780dab8d940407ecf582f
NaomyACE <122399475+Nmy81244@users.noreply.github.com>
2026-09-26 16:10:48 -0500

    Add input device interface

---
+++ core/inc/nayomi_input.h
@@ -0,0 +1,15 @@
+#ifndef NAYOMI_INPUT_H
+#define NAYOMI_INPUT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void nayomi_input_reset(void);
+void nayomi_input_sof(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
